0100d7c895bdc1911af9fad42a965465b2264f01
Nick Inman <nick@theoatmeal.com>
2018-11-21 20:40:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,13 +12,13 @@ def line(deli)
 end
 
 def take_a_number(deli, new_cust)
-  if deli.empty?
+  #if deli.empty?
     deli.push(new_cust)
     puts "Welcome, #{new_cust}. You are number #{deli.count} in line."
-  else
-    deli.push(new_cust)
-    puts "Welcome, #{new_cust}. You are number #{deli.count} in line."
-  end
+ # else
+  #  deli.push(new_cust)
+ #   puts "Welcome, #{new_cust}. You are number #{deli.count} in line."
+ #end
 end
 
 def now_serving(deli)
