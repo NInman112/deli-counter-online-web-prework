121596de4609db1d66fa7cea57e17b4f0aa45a3b
Nick Inman <nick@theoatmeal.com>
2018-11-21 21:04:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -30,7 +30,10 @@ def now_serving(deli)
 end
 
 def take_a_number(deli)
-  
+  if deli.empty?
+    puts "Welcome, #{deli}.  You are number #{deli}".
+  else
+    puts
 end
 
 new_deli = []
@@ -43,8 +46,9 @@ now_serving(new_deli) #=> "Currently serving 2."
 now_serving(new_deli) #=> "Currently serving 3."
 
 take_a_number(new_deli) #=> "Welcome, 1. You are number 1 in line." new_deli => [1]
-take_a_number(new_deli) #=> "Welcome, 2. You are number 2 in line."
-take_a_number(new_deli) #=> "Welcome, 3. You are number 3 in line."
+take_a_number(new_deli) #=> "Welcome, 2. You are number 2 in line." new_deli => [1,2]
+take_a_number(new_deli) #=> "Welcome, 3. You are number 3 in line." new_deli => [1,2,3]
 now_serving(new_deli) #=> "Currently serving 1." new_deli => [2,3]
 
 
+take_a_number(new_deli) #=> "Welcome, 4. You are number 3 in line." new_deli => [2,3,4]
